05673e0ff08075cc662d89b39d8fea10ead99690
tuuk79 <stevecyoo@gmail.com>
2017-06-28 11:16:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { GridOptions } from 'ag-grid/main';
+import { Grid } from "ag-grid/dist/lib/grid";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	private gridOptions: GridOptions;
+	public showGrid: boolean;
+	public rowData: any[];
+	private columnDefs: any[];
+
+	constructor() {
+		this.gridOptions = <GridOptions>{
+			columnDefs: this.createColumnDefs(),
+			rowData: this.createRowData()
+		};
+
+		this.showGrid = true;
+	}
+
+	// specify the columns
+	private createColumnDefs() {
+		return [
+			{ headerName: "Make", field: "make" },
+			{ headerName: "Model", field: "model" },
+			{ headerName: "Price", field: "price" }
+		];
+	}
+
+	// specify the data
+	private createRowData() {
+		return [
+			{ make: "Toyota", model: "Celica", price: 35000 },
+			{ make: "Ford", model: "Mondeo", price: 32000 },
+			{ make: "Porsche", model: "Boxter", price: 72000 }
+		];
+	}
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AgGridModule } from 'ag-grid-angular/main';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		AgGridModule.withComponents([])
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
